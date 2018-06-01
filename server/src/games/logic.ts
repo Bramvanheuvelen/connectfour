28089f163b887bd8e27bbb4169b8576c8236483f
Bram van Heuvelen <bramvanheuvelen@gmail.com>
2018-06-01 11:42:50 +0200

    all possible connect4 options working

--- server/src/games/logic.ts
+++ server/src/games/logic.ts
@@ -35,10 +35,36 @@ export const isValidTransition = (playerSymbol: Symbol, from: Board, to: Board) 
 export const calculateWinner = (board: Board): Symbol | null =>
   board
     .concat(
-      // vertical winner
-      [0, 1, 2, 3].map(n => board.map(row => row[n])) as Row[]
-      // [1, 2, 3, 4].map(n => board.map(row => row[n])) as Row[],
-      // [2, 3, 4, 5].map(n => board.map(row => row[n])) as Row[]
+      [
+      // vertical winner [0]
+      [0, 1, 2, 3].map(n => board[n][0] ),
+      [1, 2, 3, 4].map(n => board[n][0] ),
+      [2, 3, 4, 5].map(n => board[n][0] ),
+      // vertical winner [1]
+      [0, 1, 2, 3].map(n => board[n][1] ),
+      [1, 2, 3, 4].map(n => board[n][1] ),
+      [2, 3, 4, 5].map(n => board[n][1] ),
+      // vertical winner [2]
+      [0, 1, 2, 3].map(n => board[n][2] ),
+      [1, 2, 3, 4].map(n => board[n][2] ),
+      [2, 3, 4, 5].map(n => board[n][2] ),
+      // vertical winner [3]
+      [0, 1, 2, 3].map(n => board[n][3] ),
+      [1, 2, 3, 4].map(n => board[n][3] ),
+      [2, 3, 4, 5].map(n => board[n][3] ),
+      // vertical winner [4]
+      [0, 1, 2, 3].map(n => board[n][4] ),
+      [1, 2, 3, 4].map(n => board[n][4] ),
+      [2, 3, 4, 5].map(n => board[n][4] ),
+      // vertical winner [5]
+      [0, 1, 2, 3].map(n => board[n][5] ),
+      [1, 2, 3, 4].map(n => board[n][5] ),
+      [2, 3, 4, 5].map(n => board[n][5] ),
+      // vertical winner [6]
+      [0, 1, 2, 3].map(n => board[n][6] ),
+      [1, 2, 3, 4].map(n => board[n][6] ),
+      [2, 3, 4, 5].map(n => board[n][6] ),
+    ] as Row[],
     )
     .concat(
       [
@@ -74,6 +100,41 @@ export const calculateWinner = (board: Board): Symbol | null =>
         [1, 2, 3, 4].map(n => board[6-n][n]),
         [2, 3, 4, 5].map(n => board[7-n][n]),
         [3, 4, 5, 6].map(n => board[8-n][n])
+      ] as Row[]
+    )
+    .concat(
+      [
+        // horizontal winner
+        // //index [0]
+        [0].map(n => board[0][n] && board[0][n+1] && board[0][n+2] && board[0][n+3]),
+        [0].map(n => board[0][n+1] && board[0][n+2] && board[0][n+3] && board[0][n+4]),
+        [0].map(n => board[0][n+2] && board[0][n+3] && board[0][n+4] && board[0][n+5]),
+        [0].map(n => board[0][n+3] && board[0][n+4] && board[0][n+5] && board[0][n+6]),
+        //index=[1]
+        [1].map(n => board[1][n-1] && board[1][n] && board[1][n+1] && board[1][n+2]),
+        [1].map(n => board[1][n] && board[1][n+1] && board[1][n+2] && board[1][n+3]),
+        [1].map(n => board[1][n+1] && board[1][n+2] && board[1][n+3] && board[1][n+4]),
+        [1].map(n => board[1][n+2] && board[1][n+3] && board[1][n+4] && board[1][n+5]),
+        // //index=[2]
+        [2].map(n => board[2][n-2] && board[2][n-1] && board[2][n] && board[2][n+1]),
+        [2].map(n => board[2][n-1] && board[2][n] && board[2][n+1] && board[2][n+2]),
+        [2].map(n => board[2][n] && board[2][n+1] && board[2][n+2] && board[2][n+3]),
+        [2].map(n => board[2][n+1] && board[2][n+2] && board[2][n+3] && board[2][n+4]),
+        // //index=[3]
+        [3].map(n => board[3][n-3] && board[3][n-2] && board[3][n-1] && board[3][n]),
+        [3].map(n => board[3][n-2] && board[3][n-1] && board[3][n] && board[3][n+1]),
+        [3].map(n => board[3][n-1] && board[3][n] && board[3][n+1] && board[3][n+2]),
+        [3].map(n => board[3][n] && board[3][n+1] && board[3][n+2] && board[3][n+3]),
+        // //index=[4]
+        [4].map(n => board[4][n-4] && board[4][n-3] && board[4][n-2] && board[4][n-1]),
+        [4].map(n => board[4][n-3] && board[4][n-2] && board[4][n-1] && board[4][n]),
+        [4].map(n => board[4][n-2] && board[4][n-1] && board[4][n] && board[4][n+1]),
+        [4].map(n => board[4][n-1] && board[4][n] && board[4][n+1] && board[4][n+2]),
+        // //index=[5]
+        [5].map(n => board[5][n-5] && board[5][n-4] && board[5][n-3] && board[5][n-2]),
+        [5].map(n => board[5][n-4] && board[5][n-3] && board[5][n-2] && board[5][n-1]),
+        [5].map(n => board[5][n-3] && board[5][n-2] && board[5][n-1] && board[5][n]),
+        [5].map(n => board[5][n-2] && board[5][n-2] && board[5][n] && board[5][n+1])
       ] as Row[]
     )
     .filter(row => row[0] && row.every(symbol => symbol === row[0]))
